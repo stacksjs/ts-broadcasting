cee8a1241ede37daeab944fadb28fc3d931babe9
Chris <chrisbreuer93@gmail.com>
2025-10-12 16:47:34 -0700

    chore: wip

--- packages/ts-broadcasting/src/config.ts
+++ packages/ts-broadcasting/src/config.ts
@@ -1,13 +1,13 @@
-import type { BinaryConfig } from './types'
+import type { BroadcastConfig } from './types'
 import { loadConfig } from 'bunfig'
 
-export const defaultConfig: BinaryConfig = {
-  from: 'localhost:5173',
+export const defaultConfig: BroadcastConfig = {
   verbose: true,
 }
 
 // eslint-disable-next-line antfu/no-top-level-await
-export const config: BinaryConfig = await loadConfig({
-  name: 'binary',
+export const config: BroadcastConfig = await loadConfig({
+  name: 'broadcast',
+  alias: 'realtime',
   defaultConfig,
 })
